446dcffc3f0608d28a0d3b2a9e0f9c1af501940e
CraigGent <genty83@outlook.com>
2025-01-14 14:46:16 +0000

    Added core structure and configuration

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/core/components/renderer.js', // Adjust this entry point as needed
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.json$/,
+        type: 'javascript/auto',
+        use: {
+          loader: 'json-loader',
+        },
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+};
